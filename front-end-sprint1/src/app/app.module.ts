4ebc1e098f900e606424d821e075d8642490c7f8
AiYuki <khinhvu2707@gmail.com>
2021-09-11 22:08:43 +0700

    tạo component

--- front-end-sprint1/src/app/app.module.ts
+++ front-end-sprint1/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CustomerModule} from './module/customer/customer.module';
 import {GameModule} from './model/game/game.module';
 
 @NgModule({
@@ -13,7 +12,6 @@ import {GameModule} from './model/game/game.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomerModule,
     GameModule
   ],
   providers: [],
