f4c83fc1fb9863e797368091f86b8aa0a25659a9
unknown <thuythuy100897@gmail.com>
2021-09-11 22:31:57 +0700

    commit header and footer

--- front-end-sprint1/src/app/service/game/game.service.ts
+++ front-end-sprint1/src/app/service/game/game.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Game} from '../../model/game/game';
+import {GameType} from '../../model/game/game-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  public API_GAME = 'http://localhost:8080/game';
+  public API_GAME_TYPE = 'http://localhost:8080/gameType';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAllGame(): Observable<Game[]> {
+    return this.http.get<Game[]>(this.API_GAME);
+  }
+
+  saveGame(game: Game): Observable<Game> {
+    return this.http.post<Game>(this.API_GAME, game);
+  }
+
+  getAllGameType(): Observable<GameType[]> {
+    return this.http.get<GameType[]>(this.API_GAME_TYPE);
+  }
+
+  findById(id: number): Observable<Game> {
+    return this.http.get<Game>(this.API_GAME + '/' + id);
+  }
+
+  getById(id): Observable<Game[]> {
+    return this.http.get<Game[]>(this.API_GAME + '/' + id).pipe();
+  }
+
+  updateGame(id: number, game: Game): Observable<Game> {
+    return this.http.patch<Game>(this.API_GAME + '/' + id, game);
+  }
+
+  deleteGame(id: number): Observable<any> {
+    return this.http.delete(this.API_GAME + '/' + id);
+  }
+
+  search(name: string): Observable<Game[]> {
+    return this.http.get<Game[]>(this.API_GAME + '?name_like=' + name);
+  }
 }
