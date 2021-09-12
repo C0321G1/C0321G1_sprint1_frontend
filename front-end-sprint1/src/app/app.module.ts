a2e7ca8ab61f9cd24e647d0b42042600fb3e5451
Hau HP <hauhpdn2015@gmail.com>
2021-09-12 21:53:27 +0700

    import module

--- front-end-sprint1/src/app/app.module.ts
+++ front-end-sprint1/src/app/app.module.ts
@@ -1,9 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {CustomerModule} from './module/customer/customer.module';
+import {AccountModule} from './module/account/account.module';
+import {ComputerModule} from './module/computer/computer.module';
+import {EmployeeModule} from './module/employee/employee.module';
+import {GameModule} from './module/game/game.module';
+import {OrderDetailModule} from './module/order-detail/order-detail.module';
+import {ServiceModule} from './module/service/service.module';
+import {RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +20,17 @@ import {CustomerModule} from './module/customer/customer.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomerModule
+    CustomerModule,
+    AccountModule,
+    ComputerModule,
+    EmployeeModule,
+    GameModule,
+    OrderDetailModule,
+    ServiceModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
