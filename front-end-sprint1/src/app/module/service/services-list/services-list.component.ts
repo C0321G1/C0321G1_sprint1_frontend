e55f1bdd23a133f6ddfd990d3779c72bfec9de3a
phikhanhb7 <phikhanhb7@gmail.com>
2021-09-16 23:11:19 +0700

    lan cuoi nhe

--- front-end-sprint1/src/app/module/service/services-list/services-list.component.ts
+++ front-end-sprint1/src/app/module/service/services-list/services-list.component.ts
@@ -25,7 +25,7 @@ export class ServicesListComponent implements OnInit {
   public checkbox2 = true;
   public inputPage = '';
   public deleteServicesId: Array<number> = [];
-  public deleteServicesCode: Array<String> = [];
+  public deleteServicesCode: Array<string> = [];
 
   ps: Array<any> = [];
 
@@ -42,7 +42,7 @@ export class ServicesListComponent implements OnInit {
   // phap
   deleteArr(id: number, code: string) {
     for (let i = 0; i < this.deleteServicesId.length; i++) {
-      if (this.deleteServicesId[i] == id) {
+      if (this.deleteServicesId[i] === id) {
         this.deleteServicesId.splice(i, 1);
         this.deleteServicesCode.splice(i, 1);
         return;
@@ -50,8 +50,8 @@ export class ServicesListComponent implements OnInit {
     }
     this.deleteServicesId.push(id);
     this.deleteServicesCode.push(code);
-    console.log("code "+this.deleteServicesCode);
-    console.log("id "+this.deleteServicesId);
+    console.log('code ' + this.deleteServicesCode);
+    console.log('id ' + this.deleteServicesId);
   }
 
 
@@ -61,20 +61,20 @@ export class ServicesListComponent implements OnInit {
       this.check = 0;
       if (value == null) {
         this.servicesPage = [];
-        this.messenger = "List is empty";
+        this.messenger = 'List is empty';
         this.p = 0;
       }
       this.servicesPage = value.content;
       for (let i = 0; i < this.servicesPage.length; i++) {
         for (let j = 0; j < this.deleteServicesId.length; j++) {
-          if (this.servicesPage[i].servicesId===this.deleteServicesId[j]){
-           this.servicesPage[i].flag=3;
+          if (this.servicesPage[i].servicesId === this.deleteServicesId[j]) {
+            this.servicesPage[i].flag = 3;
             break;
           }
         }
       }
       this.ps = new Array<any>(value.totalPages);
-      this.name='';
+      this.name = '';
       console.log(this.servicesPage);
     }, error => {
       console.log(error);
@@ -87,14 +87,14 @@ export class ServicesListComponent implements OnInit {
       this.check = 1;
       if (value == null) {
         this.servicesPage = [];
-        this.messenger = "List is empty";
+        this.messenger = 'List is empty';
         this.p = 0;
       }
       this.servicesPage = value.content;
       for (let i = 0; i < this.servicesPage.length; i++) {
         for (let j = 0; j < this.deleteServicesId.length; j++) {
-          if (this.servicesPage[i].servicesId===this.deleteServicesId[j]){
-            this.servicesPage[i].flag=3;
+          if (this.servicesPage[i].servicesId === this.deleteServicesId[j]) {
+            this.servicesPage[i].flag = 3;
             break;
           }
         }
@@ -157,7 +157,7 @@ export class ServicesListComponent implements OnInit {
   next() {
     console.log(this.ps.length);
     if (this.p > this.ps.length - 2) {
-      if (this.check == 0) {
+      if (this.check === 0) {
         this.p = this.ps.length - 1;
         this.getAllServicesList();
       } else {
@@ -180,36 +180,36 @@ export class ServicesListComponent implements OnInit {
 
   }
 
-  //phap
+  // phap
   openDialog(id: any): void {
-        this.serviceService.findById(id).subscribe(dataDialog => {
-          const dialogRef = this.dialog.open(DeleteServicesComponent, {
-            width: '500px',
-            data: {name: dataDialog},
-            disableClose: true
-          });
-          dialogRef.afterClosed().subscribe(result => {
-            this.ngOnInit();
-          });
-        });
+    this.serviceService.findById(id).subscribe(dataDialog => {
+      const dialogRef = this.dialog.open(DeleteServicesComponent, {
+        width: '500px',
+        data: {name: dataDialog},
+        disableClose: true
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        this.ngOnInit();
+      });
+    });
   }
 
 // phap
   openDialogAll(): void {
-      const dialogRef = this.dialog.open(DeleteServicesComponent, {
-        width: '500px',
-        data: {id:this.deleteServicesId,code: this.deleteServicesCode },
-        disableClose: true
-      });
-      dialogRef.afterClosed().subscribe(result => {
-        if (!result){
+    const dialogRef = this.dialog.open(DeleteServicesComponent, {
+      width: '500px',
+      data: {id: this.deleteServicesId, code: this.deleteServicesCode},
+      disableClose: true
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (!result) {
 
-        }else {
-          this.deleteServicesId=[];
-          this.deleteServicesCode=[];
-        }
-        this.ngOnInit();
-      });
+      } else {
+        this.deleteServicesId = [];
+        this.deleteServicesCode = [];
+      }
+      this.ngOnInit();
+    });
   }
 
 
@@ -220,7 +220,7 @@ export class ServicesListComponent implements OnInit {
       this.first();
     } else if (Number(event.target.value) > this.ps.length - 1) {
       this.last();
-    } else if (this.check == 0) {
+    } else if (this.check === 0) {
       this.p = (Number(event.target.value) - 1);
       this.getAllServicesList();
     } else {
@@ -231,9 +231,9 @@ export class ServicesListComponent implements OnInit {
   }
 
   resetForm() {
-    this.code='';
-    this.name2='';
-    this.prices='';
-    this.name='';
+    this.code = '';
+    this.name2 = '';
+    this.prices = '';
+    this.name = '';
   }
 }
