2ed075859ff34a04fe87c2c3c0d6c33a73ca2e9f
phikhanhb7 <phikhanhb7@gmail.com>
2021-09-19 17:18:57 +0700

    up

--- front-end-sprint1/src/app/module/computer/computer-routing.module.ts
+++ front-end-sprint1/src/app/module/computer/computer-routing.module.ts
@@ -1,20 +1,19 @@
-
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule, Routes} from "@angular/router";
-import {CreateComputerComponent} from "./create-computer/create-computer.component";
-import {EditComputerComponent} from "./edit-computer/edit-computer.component";
-import {ComputerListComponent} from "./computer-list/computer-list.component";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {CreateComputerComponent} from './create-computer/create-computer.component';
+import {EditComputerComponent} from './edit-computer/edit-computer.component';
+import {ComputerListComponent} from './computer-list/computer-list.component';
 
 /*long-computer*/
-const routes : Routes = [
-  {path : 'create-computer',component : CreateComputerComponent},
-  {path : 'update-computer/:id',component: EditComputerComponent},
+const routes: Routes = [
+  {path: 'create-computer', component: CreateComputerComponent},
+  {path: 'update-computer/:id', component: EditComputerComponent},
   {
     path: 'computer-list',
     component: ComputerListComponent
   }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
